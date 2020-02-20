87bc7f400d9f96ae9e4a3a4e6d88e75c916519a7
Jorg Nieberg <jnieberg@wundermancommerce.com>
2020-02-20 14:08:16 +0100

    update

--- js/Bloodwych.js
+++ js/Bloodwych.js
@@ -447,6 +447,12 @@ $(function () {
 	}, false);
 	setViewportScale();
 
+	screen.orientation.lock('landscape').then(function success() {
+		console.log('Successfully locked the orientation');
+	}, function error(errMsg) {
+		console.log('Error locking the orientation :: ' + errMsg);
+	});
+
 	$(document).focusin(function (e) {
 		var t = $(e.target);
 		if (gameStarted && paused && pausedByBrowser) {
@@ -575,4 +581,3 @@ $(function () {
 		canvas.focus();
 	});
 });
-
